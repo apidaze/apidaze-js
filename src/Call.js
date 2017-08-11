8f33ba542b6c721b2d23d9d3b1b59965f537a8b8
Philippe Sultan <phsultan@apidaze.io>
2017-08-12 00:54:04 +0200

    Replaced function to generate UUIDs

--- src/Call.js
+++ src/Call.js
@@ -257,18 +257,8 @@ function createPeerConnection(){
   }
 }
 
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-};
-
 function startCall(){
-  var callID = guid();
+  var callID = generateGUID();
   var request = {};
   request.wsp_version = "1";
   request.method = "call";
@@ -368,5 +358,33 @@ function handleGUMSuccess(stream){
 function handleGUMError(error){
   this._onError(error)
 }
+
+var generateGUID = (typeof(window.crypto) !== 'undefined' && typeof(window.crypto.getRandomValues) !== 'undefined') ?
+function() {
+    // If we have a cryptographically secure PRNG, use that
+    // http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
+    var buf = new Uint16Array(8);
+    window.crypto.getRandomValues(buf);
+    var S4 = function(num) {
+        var ret = num.toString(16);
+        while (ret.length < 4) {
+            ret = "0" + ret;
+        }
+        return ret;
+    };
+    return (S4(buf[0]) + S4(buf[1]) + "-" + S4(buf[2]) + "-" + S4(buf[3]) + "-" + S4(buf[4]) + "-" + S4(buf[5]) + S4(buf[6]) + S4(buf[7]));
+}
+
+:
+
+function() {
+    // Otherwise, just use Math.random
+    // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+};
 
 export default Call;
