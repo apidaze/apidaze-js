666844e5a18cceec5651711f14df04d43f6f04a6
Philippe Sultan <phsultan@apidaze.io>
2017-09-18 15:51:48 +0200

    First set of sample files to test JS API with.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 var childProcess = require('child_process');
 var webpack = require('webpack');
 var PKG_VERSION = require('./package.json').version;
 var GITBRANCH = childProcess.execSync('git branch | grep \\* | cut -d \" \" -f2').toString().trim();
 var VERSIONSTR = PKG_VERSION + "-dev-" + GITBRANCH;
+var APIDAZE_JS_FILENAME = "APIdaze-" + VERSIONSTR + ".js";
 
 module.exports = {
   entry: './src/index.js',
@@ -33,6 +37,25 @@ module.exports = {
         'process.env.VERSIONSTR': JSON.stringify(VERSIONSTR),
         'process.env.PRODUCTION': JSON.stringify(false),
         'process.env.DEVELOPMENT': JSON.stringify(true),
+        'process.env.APIDAZE_JS_FILENAME': JSON.stringify(APIDAZE_JS_FILENAME)
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'APIdaze Samples',
+      filename: 'samples/index.html',
+      template: 'html_webpack_plugin_templates/index.template.ejs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Anonymous login',
+      filename: 'samples/anonymous_login/index.html',
+      template: 'html_webpack_plugin_templates/anonymous_login/index.template.ejs',
+      inject: 'body',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Check User ID from External Script',
+      filename: 'samples/external_script_login/index.html',
+      template: 'html_webpack_plugin_templates/external_script_login/index.template.ejs',
+      inject: 'body',
     })
   ]
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 var PKG_VERSION = require('./package.json').version;
 var GITBRANCH = childProcess.execSync('git branch | grep \\* | cut -d \" \" -f2').toString().trim();
 var VERSIONSTR = PKG_VERSION;
+var APIDAZE_JS_FILENAME = "APIdaze-" + VERSIONSTR + ".js";
 
 module.exports = {
   entry: './src/index.js',
@@ -33,6 +34,7 @@ module.exports = {
         'process.env.VERSIONSTR': JSON.stringify(VERSIONSTR),
         'process.env.PRODUCTION': JSON.stringify(true),
         'process.env.DEVELOPMENT': JSON.stringify(false),
+        'process.env.APIDAZE_JS_FILENAME': JSON.stringify(APIDAZE_JS_FILENAME)
     })
   ]
 };
