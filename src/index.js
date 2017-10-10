c273c5f398d52713a64eeeb930e5b343a851d32a
Philippe Sultan <phsultan@apidaze.io>
2017-10-10 21:46:36 +0200

    Added APIdaze.isWebRTCSupported() and APIdaze.browerDetails() functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import CLIENT from './CLIENT.js';
+import * as WebRTCAdapter from 'webrtc-adapter';
+import Logger from './Logger.js';
 
 var __VERSION__ = process.env.VERSIONSTR // webpack defineplugin variable
-
 var version = __VERSION__
+
+var LOG_PREFIX = 'APIdaze-' + __VERSION__;
+
+const isWebRTCSupported = function() {
+  return  typeof WebRTCAdapter.browserDetails === "object" &&
+          typeof WebRTCAdapter.browserShim === "object";
+}
+
+const browserDetails = function() {
+  return WebRTCAdapter.browserDetails;
+}
 
 export {
   version,
+  isWebRTCSupported,
+  browserDetails,
   CLIENT
 }
