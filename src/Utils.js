ad6046ee4e0459479655d7c3871317e05052f8ae
Maciej <maciej.kasprzyk@impaqgroup.com>
2019-04-10 10:01:34 +0200

    style: run prettier

--- src/Utils.js
+++ src/Utils.js
@@ -1,32 +1,47 @@
 var Utils = {};
 
-Utils.generateGUID = (typeof window === "object" && typeof(window.crypto) !== 'undefined' && typeof(window.crypto.getRandomValues) !== 'undefined') ?
-function() {
-    // If we have a cryptographically secure PRNG, use that
-    // http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
-    var buf = new Uint16Array(8);
-    window.crypto.getRandomValues(buf);
-    var S4 = function(num) {
-        var ret = num.toString(16);
-        while (ret.length < 4) {
+Utils.generateGUID =
+  typeof window === "object" &&
+  typeof window.crypto !== "undefined" &&
+  typeof window.crypto.getRandomValues !== "undefined"
+    ? function() {
+        // If we have a cryptographically secure PRNG, use that
+        // http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
+        var buf = new Uint16Array(8);
+        window.crypto.getRandomValues(buf);
+        var S4 = function(num) {
+          var ret = num.toString(16);
+          while (ret.length < 4) {
             ret = "0" + ret;
-        }
-        return ret;
-    };
-    return (S4(buf[0]) + S4(buf[1]) + "-" + S4(buf[2]) + "-" + S4(buf[3]) + "-" + S4(buf[4]) + "-" + S4(buf[5]) + S4(buf[6]) + S4(buf[7]));
-}
-
-:
-
-function() {
-    // Otherwise, just use Math.random
-    // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
-        v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-};
+          }
+          return ret;
+        };
+        return (
+          S4(buf[0]) +
+          S4(buf[1]) +
+          "-" +
+          S4(buf[2]) +
+          "-" +
+          S4(buf[3]) +
+          "-" +
+          S4(buf[4]) +
+          "-" +
+          S4(buf[5]) +
+          S4(buf[6]) +
+          S4(buf[7])
+        );
+      }
+    : function() {
+        // Otherwise, just use Math.random
+        // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(
+          c
+        ) {
+          var r = (Math.random() * 16) | 0,
+            v = c == "x" ? r : (r & 0x3) | 0x8;
+          return v.toString(16);
+        });
+      };
 
 /**
  * Generates an unsigned 32 bits integer out of a string.
@@ -34,8 +49,11 @@ function() {
  * Needed to map FreeSWITCH room name to Janus as Janus only accepts
  * positive integers as room identifiers
  */
-Utils.hashCode = function(s){
-  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
-}
+Utils.hashCode = function(s) {
+  return s.split("").reduce(function(a, b) {
+    a = (a << 5) - a + b.charCodeAt(0);
+    return a & a;
+  }, 0);
+};
 
-export default Utils
+export default Utils;
