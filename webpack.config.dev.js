90120a65b99da9bba0336999cf0f78930c5c5781
Maciej <maciej.kasprzyk@impaqgroup.com>
2019-05-13 22:00:04 +0200

    chore: remove branch from build filenames

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,7 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-var childProcess = require('child_process');
 var webpack = require('webpack');
 var PKG_VERSION = require('./package.json').version;
-var GITBRANCH = childProcess.execSync('git branch | grep \\* | cut -d \" \" -f2').toString().trim();
-var VERSIONSTR = PKG_VERSION + "-dev-" + GITBRANCH;
+var VERSIONSTR = PKG_VERSION + "-dev";
 var APIDAZE_JS_FILENAME = "APIdaze-" + VERSIONSTR + ".js";
 
 module.exports = {
@@ -34,7 +30,6 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-        'process.env.GITBRANCH': JSON.stringify(GITBRANCH),
         'process.env.VERSIONSTR': JSON.stringify(VERSIONSTR),
         'process.env.PRODUCTION': JSON.stringify(false),
         'process.env.DEVELOPMENT': JSON.stringify(true),
